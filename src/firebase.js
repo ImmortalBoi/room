284f03f0c0d91f1df5f0735f3892008167ec56f2
Khaled Eldesoukey <54212167+ImmortalBoi@users.noreply.github.com>
2024-01-07 06:23:55 +0200

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -6,17 +6,6 @@ import { getAuth } from "firebase/auth";
 // Your web app's Firebase configuration
 
 export const firebaseApp = initializeApp({
-    apiKey: 'AIzaSyBCIcpxzM2wUmfMvliofmRQoUyTm9x0C7k',
-  
-    authDomain: 'room-4d562.firebaseapp.com',
-  
-    projectId: 'room-4d562',
-  
-    storageBucket: 'room-4d562.appspot.com',
-  
-    messagingSenderId: '1059530472742',
-  
-    appId: '1:1059530472742:web:18e87f1f3793e4733ed5e7'
   })
 
 // used for the firestore refs
